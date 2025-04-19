a59f6d1e4d0f93c2d799b531d0734001024ffca1
Bhaskar Sharma <bhaskarneeraj23@gmail.com>
2025-04-19 20:55:00 +0530

    ADD: create index exports for components and pages; update routes to import Flights from index

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+
+export {default as "404NotFound"} from './404NotFound';
+export {default as BannerSection} from './bannerSection';
+export {default as Typography   } from "./ui/styledTypography"
+export {default as FlightExplorerSection} from './FlightExplorer';
